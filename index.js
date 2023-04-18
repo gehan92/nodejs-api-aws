785554ec1657236c388d661f01687970e6065864
P A Gehan ishanka <36468544+gehan92@users.noreply.github.com>
2023-04-18 11:17:24 +0530

    update core section

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const Cors = require('cors')
 const routes = require('./src/routes');
 
 app.use(express.json());
-app.use(Cors());
+
+app.use(Cors({
+    origin:"http://localhost:3000"
+}));
+
+
 app.use('/',routes)
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Listing to port ${port}`);
 })
